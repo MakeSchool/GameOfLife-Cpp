a9ed5a2a97fa192251428e64005ffcb648359a3d
Daniel Haaser <thedapperdan@gmail.com>
2015-02-12 11:46:56 -0800

    HelloWorldScene -> MainScene

--- GameOfLife/Classes/MainScene.h
+++ GameOfLife/Classes/MainScene.h
@@ -1,9 +1,9 @@
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __MAIN_SCENE_H__
+#define __MAIN_SCENE_H__
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::Layer
+class MainScene : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
@@ -13,7 +13,7 @@ public:
     virtual bool init();
 
     // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+    CREATE_FUNC(MainScene);
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif // __MAIN_SCENE_H__
